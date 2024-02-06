fa61497d6e9933861b5cff7a5a5c0a7a8b6f7298
VincentNoc <kst0629@autuni.ac.nz>
2024-02-06 16:53:23 +1300

    Start AVSCAN
    this is the starting of the AVSCAN application.

---
+++ AVSCAN/src/main/java/com/mycompany/avscan/AVSCAN.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.avscan;
+
+/**
+ *
+ * @author vince-kong
+ */
+public class AVSCAN {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
