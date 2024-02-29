9cf9198e03fe6d303249a0a52a46a1af91bd5ba9
Dmitry Kirov <mdd2557@autuni.ac.nz>
2024-02-29 20:38:00 +1300

    GUI finished. fix delete row button

--- AVSCAN/src/main/java/com/mycompany/avscan/NewEq.java
+++ AVSCAN/src/main/java/com/mycompany/avscan/NewEq.java
@@ -17,110 +17,147 @@ import javax.swing.table.TableColumn;
  * @author dmitr
  */
 public class NewEq {
+
     private int width;
     private int height;
-    
     private JFrame frame;
+    //private JPanel page;
+
+    //private JPanel inputArea;
+    //private JPanel inputSection[];
     private JLabel[] inuptLabels;
     private JTextField[] inputField;
     private JScrollPane showInfo;
+
+    private JTable table;
+    private TableColumn buttonColumn;
+    private JScrollPane scrollPane;
+
+    // private JPanel buttons;
+    private JButton controls[];
+
+    ;
     
     public static void main(String[] args) {//Just to start the page
         new NewEq();
     }
-    
-    public NewEq(){
-        
+
+    public NewEq() {
+
         creatingFrame();
     }
-    
-    private void creatingFrame(){
+
+    private void creatingFrame() {
+        /*this.page = new JPanel();
+        this.page.setLayout(new BoxLayout(page, BoxLayout.Y_AXIS));*/
         this.frame = new JFrame("New Equipment");
         this.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        
+
         GraphicsDevice g = GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice();
-        width = g.getDisplayMode().getWidth()+14;
-        height = g.getDisplayMode().getHeight()-33;
-        System.out.println("width = "+ width+"AND height = "+this.height);
+        width = g.getDisplayMode().getWidth() + 14;
+        height = g.getDisplayMode().getHeight() - 32;
+        System.out.println("width = " + width + "AND height = " + this.height);
         this.frame.setBounds(-7, 0, width, height);
-        
-        creatingInputPanels();
+        //this.page.setPreferredSize(new Dimension(width, height));
+
+        creatingInputs();
         addedEqPannel();
-        
+        addingButtons();
+
         JPanel temp = new JPanel();
         this.frame.add(temp);
-        
+
 //        frame.getContentPane().add(showInfo, BorderLayout.CENTER);
+        //this.frame.add(this.page);
         this.frame.setResizable(false);
+        //this.frame.pack();
         this.frame.setVisible(true);
     }
-    
-    private void creatingInputPanels(){
+
+    private void creatingInputs() {
+        /*this.inputArea = new JPanel();//new BorderLayout()
+        this.inputArea.setLayout(new BoxLayout(inputArea, BoxLayout.X_AXIS)); 
+        this.inputArea.setBorder(BorderFactory.createLineBorder(Color.BLUE));
+        this.inputArea.setPreferredSize(new Dimension(400, 40));
+        
+        this.inputSection = new JPanel[3];*/
+        //this.inputArea.setForeground(Color.BLUE);
         this.inuptLabels = new JLabel[3];
         this.inputField = new JTextField[3];
-        String[] inputHeaders ={"Equipment ID", "Equipment Name", "Equipment Type"};
-        
-        for(int i =0; i<3;i++){
-                    //.setBounds(x, y, width, height);
+        String[] inputHeaders = {"Equipment ID", "Equipment Name", "Equipment Type"};
+
+        for (int i = 0; i < 3; i++) {
+            //.setBounds(x, y, width, height);
             this.inuptLabels[i] = new JLabel(inputHeaders[i]);
-            this.inuptLabels[i].setBounds(i*600+50, i+10, 285, 35);
+            this.inuptLabels[i].setBounds(i * 600 + 50, 10, 285, 35);
             this.inputField[i] = new JTextField();
-            this.inputField[i].setBounds(i*600+50, i+50, 330, 35);
-            System.out.println("inputField["+i+"].setBounds("+(i*600+50)+", "+(i+50)+", "+285+", "+35+")");
-            
+            this.inputField[i].setBounds(i * 600 + 50, i + 50, 330, 35);
+            System.out.println("inputField[" + i + "].setBounds(" + (i * 600 + 50) + ", " + (i + 50) + ", " + 285 + ", " + 35 + ")");
+
+            /*this.inputSection[i] = new JPanel();
+            this.inputSection[i].setLayout(new BoxLayout(inputSection[i], BoxLayout.Y_AXIS));
+            this.inputSection[i].add(inuptLabels[i]);
+            this.inputSection[i].add(inputField[i]);
+            this.inputSection[i].setBorder(BorderFactory.createLineBorder(Color.RED));*/
+ /*this.inputArea.add(inuptLabels[i]);
+            this.inputArea.add(this.inputField[i]);*/
             this.frame.add(this.inuptLabels[i]);
             this.frame.add(this.inputField[i]);
-            
-        }
-    }
-    
-    private void addedEqPannel() {
 
+        }
+        /*this.inputArea.add(inputSection[0], BorderLayout.WEST);
+        this.inputArea.add(inputSection[1], BorderLayout.CENTER);
+        this.inputArea.add(inputSection[2], BorderLayout.EAST);
+        this.inputArea.setPreferredSize(new Dimension(width,10));
+        this.page.add(inputArea);*/
+        //this.frame.add(inputArea);
+    }
+
+    private void addedEqPannel() {
         // Create a table model with three columns
         DefaultTableModel tableModel = new DefaultTableModel();
         tableModel.addColumn("Name");
         tableModel.addColumn("ID");
-        tableModel.addColumn("Close");
+        tableModel.addColumn("Delete");
+        //tableModel.addColumn(new JButton("Delete"));
 
         // Add some sample data
-        for (int i = 0; i<100;i++){
-            tableModel.addRow(new Object[]{"Data "+i+"A","Data "+i+"B", "Close"});
+        for (int i = 0; i < 100; i++) {
+            tableModel.addRow(new Object[]{"Data " + i + "A", "Data " + i + "B", "Delete"});
         }
-        tableModel.addRow(new Object[]{"Data 1A", "Data 1B", "Button 1"});
-        tableModel.addRow(new Object[]{"Data 2A", "Data 2B", "Button 2"});
-        tableModel.addRow(new Object[]{"Data 3A", "Data 3B", "Button 3"});
 
         // Create a JTable with the model
-        JTable table = new JTable(tableModel);
+        table = new JTable(tableModel);
 
         // Set the size of the table
         table.setBounds(50, 300, 1000, 200);
 
         // Create a button column renderer
-        TableColumn buttonColumn = table.getColumn("Close");
+        buttonColumn = table.getColumn("Delete");
         buttonColumn.setCellRenderer(new ButtonRenderer());
 
         // Add action listener to the button column
-        buttonColumn.setCellEditor(new ButtonEditor(new JTextField(), table));
+        buttonColumn.setCellEditor(new ButtonEditor(new JTextField(), table, tableModel));
 
         // Create a JScrollPane and add the table to it
-        JScrollPane scrollPane = new JScrollPane(table);
-        scrollPane.setBounds(50, 300, this.width-155, 200);
+        scrollPane = new JScrollPane(table);
+        scrollPane.setBounds(50, 300, this.width - 155, 200);
 
         // Add the scroll pane to the frame
         frame.add(scrollPane);
-
+        //this.page.add(scrollPane);
     }
 
     // Custom renderer for the button column
     static class ButtonRenderer extends JButton implements TableCellRenderer {
+
         public ButtonRenderer() {
             setOpaque(true);
         }
 
         @Override
         public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected,
-                                                       boolean hasFocus, int row, int column) {
+                boolean hasFocus, int row, int column) {
             setText((value == null) ? "" : value.toString());
             return this;
         }
@@ -128,18 +165,24 @@ public class NewEq {
 
     // Custom editor for the button column
     static class ButtonEditor extends DefaultCellEditor {
+
         private JButton button;
         private JTable table;
+        private DefaultTableModel tableModel;
 
-        public ButtonEditor(JTextField textField, JTable table) {
+        public ButtonEditor(JTextField textField, JTable table, DefaultTableModel tableModel) {
             super(textField);
-            this.table= table;
+            this.table = table;
+            this.tableModel = tableModel;
             button = new JButton();
             button.addActionListener(new ActionListener() {
                 @Override
                 public void actionPerformed(ActionEvent e) {
                     // Handle button click event
-                    JOptionPane.showMessageDialog(null, "Button clicked in row " + table.getSelectedRow());
+                    int selectedRow = table.getSelectedRow();
+                    if (selectedRow != -1) {
+                        tableModel.removeRow(selectedRow);
+                    }
                 }
             });
         }
@@ -150,5 +193,21 @@ public class NewEq {
             return button;
         }
     }
-    
+
+    private void addingButtons() {
+        String buttonText[] = {"Home", "Add", "Format"};
+        this.controls = new JButton[3];
+        /*this.buttons = new JPanel();
+        this.buttons.add(home);
+        this.buttons.add(addButton);
+        this.buttons.add(format);
+        this.buttons.setLocation(400, 400);
+        this.page.add(buttons);*/
+
+        for (int i = 0; i < 3; i++) {
+            controls[i]= new JButton(buttonText[i]);
+            this.controls[i].setBounds(i * 600 + 50, 900, 330, 35);
+            this.frame.add(controls[i]);
+        }
+    }
 }
