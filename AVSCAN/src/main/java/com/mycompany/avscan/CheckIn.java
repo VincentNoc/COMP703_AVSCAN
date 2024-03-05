751ca797766224f9d0b1993addac07716b33e441
VincentNoc <vk6953@hotmail.com>
2024-03-05 14:16:01 +1300

    Feat: output mysql data into JTable
    Summary:
    Currently making a method that will fetch data from the mysql database, to output into the jTable.

--- AVSCAN/src/main/java/com/mycompany/avscan/CheckIn.java
+++ AVSCAN/src/main/java/com/mycompany/avscan/CheckIn.java
@@ -192,6 +192,7 @@ public class CheckIn extends javax.swing.JFrame {
     }
     
     
+    
     private void jTextField1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jTextField1ActionPerformed
         // TODO add your handling code here:
     }//GEN-LAST:event_jTextField1ActionPerformed
--- AVSCAN/src/main/java/com/mycompany/avscan/DatabaseUtils.java
+++ AVSCAN/src/main/java/com/mycompany/avscan/DatabaseUtils.java
@@ -11,16 +11,22 @@ import java.sql.SQLException;
 import java.sql.ResultSet;
 import java.sql.Statement;
 
+
 /**
  *
  * @author vince-kong
  */
+
 public class DatabaseUtils {
   Connection con;
+  private final String URL="jdbc:mysql://localhost:3306/mysql";
+  private final String USER= "root";
+  private final String PASSWORD = "AUT4events_";
 
   public DatabaseUtils() throws SQLException {
     this.con = DatabaseConnector.connectToDatabase();
   }
+  
   public DatabaseUtils(String equipmentID, String equipmentName, String equipmentType) {
     insertData(equipmentID, equipmentName, equipmentType);
   }
@@ -29,17 +35,15 @@ public class DatabaseUtils {
     String query = "INSERT INTO EquipmentLog (EquipmentID, EquipmentName, EquipmentType) VALUES (?, ?, ?)";
     try {
       Class.forName("com.mysql.cj.jdbc.Driver");
-      Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/mysql", "root", "AUT4events_");
+      Connection con = DriverManager.getConnection(URL, USER, PASSWORD);
       System.out.println("Connected to Database");
-
-      ////here sonoo is database name, root is username and password  
+      
       Statement stmt = con.createStatement();
-      PreparedStatement prepStmt = con.prepareStatement("insert into EquipmentLog (EquipmentID, EquipmentName, EquipmentType)" + " values (?, ?, ?)");
+      PreparedStatement prepStmt = con.prepareStatement(query);
       prepStmt.setString(1, equipmentID);
       prepStmt.setString(2, equipmentName);
       prepStmt.setString(3, equipmentType);
       prepStmt.execute();
-      ResultSet rs = stmt.executeQuery("select * from ITEMS");
 
       System.out.println("Information added");
       con.close();
@@ -49,7 +53,28 @@ public class DatabaseUtils {
     }
   }
 
-  public void showTable() {
+  public void fetchDataFromDatabase() {
+      
+    try {
+      Class.forName("com.mysql.cj.jdbc.Driver");
+      Connection con = DriverManager.getConnection(URL, USER, PASSWORD);
+      System.out.println("Connected to Database");
 
+      
+      Statement stmt = con.createStatement();
+      String query ="SELECT * FROM EquipmentLog";
+      ResultSet rs = stmt.executeQuery(query);
+
+      while(rs.next()){
+          String equipmentID = rs.getString("EquipmentID");
+          String equipmentName = rs.getString("EquipmentName");
+          String equipmentType = rs.getString("EquipmentType");
+      }
+    } catch (Exception e) {
+        System.err.println("Error!");
+        System.err.println("Was not able to retrieve data from database.");
+        e.printStackTrace();
+      
+    }
   }
 }
