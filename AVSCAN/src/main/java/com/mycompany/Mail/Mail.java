7f010296f262ae635cca5532106e0eb01615e5ec
VincentNoc <vk6953@hotmail.com>
2024-04-03 12:26:24 +1300

    Mail
    Code for email is being made.

--- AVSCAN/src/main/java/com/mycompany/Mail/Mail.java
+++ AVSCAN/src/main/java/com/mycompany/Mail/Mail.java
@@ -4,37 +4,78 @@
  */
 package com.mycompany.Mail;
 
+import java.io.IOException;
 import java.util.Properties;
+import javax.mail.Message;
+import javax.mail.MessagingException;
 import javax.mail.Session;
-
+import javax.mail.Transport;
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
+import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
+import java.util.Properties;
+import javax.mail.*;
+import javax.mail.internet.*;
 /**
  *
  * @author vince-kong
  */
+
+
 public class Mail {
     Session newSession = null;
-    public static void main(String args[]){
+    MimeMessage mimeMessage = null;
+
+    public static void main(String args[]) {
         Mail mail = new Mail();
-        mail.setUpServerProperties();
-        mail.draftEmail();
-        mail.sendEmail();
+        try {
+            mail.setUpServerProperties();
+            mail.draftEmail();
+            mail.sendEmail();
+        } catch (MessagingException e) {
+            e.printStackTrace();
+            System.out.println("Failed to send email: " + e.getMessage());
+        }
     }
 
     private void setUpServerProperties() {
-        Properties prop = System.getProperties();
-        prop.put("mail.smtp.port", "547");
+        Properties prop = new Properties();
+        prop.put("mail.smtp.port", "587");
         prop.put("mail.smtp.auth", "true");
         prop.put("mail.smtp.starttls.enable", "true");
-        newSession = Session.getDefaultInstance(prop, null);
-
+        newSession = Session.getDefaultInstance(prop, new Authenticator() {
+            @Override
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication("Vk6953@hotmail.com", "Dtmvzw581");
+            }
+        });
     }
 
-    private void draftEmail() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    private void draftEmail() throws MessagingException {
+        String[] emailReceipients = {"kst0629@autuni.ac.nz"};
+        String emailSubject = "Test Mail";
+        String emailBody = "Test Body of my email";
+        mimeMessage = new MimeMessage(newSession);
+        for (int i = 0; i < emailReceipients.length; i++) {
+            mimeMessage.addRecipient(Message.RecipientType.TO, new InternetAddress(emailReceipients[i]));
+        }
+        mimeMessage.setSubject(emailSubject);
+        MimeBodyPart bodyPart = new MimeBodyPart();
+        bodyPart.setContent(emailBody, "text/html");
+        Multipart multiPart = new MimeMultipart();
+        multiPart.addBodyPart(bodyPart);
+        mimeMessage.setContent(multiPart);
     }
 
-    private void sendEmail() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    private void sendEmail() throws MessagingException {
+        String emailHost = "smtp-mail.outlook.com";
+        try (Transport transport = newSession.getTransport("smtp")) {
+            transport.connect(emailHost, "Vk6953@hotmail.com", "Dtmvzw581");
+            transport.sendMessage(mimeMessage, mimeMessage.getAllRecipients());
+            System.out.println("Email successfully sent!!!");
+        }
     }
-    
 }
+
