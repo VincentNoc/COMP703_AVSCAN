a1530739d22ca5e436e59c530e5c8cdbbe74c214
VincentNoc <vk6953@hotmail.com>
2024-05-14 14:11:59 +1200

    feat:dark-mode
    Summary: What was done was that I downloaded the jar file for flatlaf so that I can create a dark mode feature for AVSCAN.

--- AVSCAN/src/main/java/com/mycompany/avscan/MainMenu.java
+++ AVSCAN/src/main/java/com/mycompany/avscan/MainMenu.java
@@ -9,6 +9,7 @@ package com.mycompany.avscan;
  * @author 2xkaz
  */
 import com.mycompany.avscan.Login_Signup_pages.LoginPage;
+import javax.swing.UIManager;
 public class MainMenu extends javax.swing.JFrame {
 
     /**
@@ -246,22 +247,10 @@ public class MainMenu extends javax.swing.JFrame {
          * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
          */
         try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(MainMenu.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(MainMenu.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(MainMenu.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(MainMenu.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            UIManager.setLookAndFeel( new FlatLightLaf() );
+        } catch( Exception ex ) {
+            System.err.println( "Failed to initialize LaF" );
         }
-        //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
