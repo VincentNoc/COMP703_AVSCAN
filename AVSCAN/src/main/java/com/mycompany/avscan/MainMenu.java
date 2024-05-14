7331cdee2baf92079bd663839087b6485c6d5c41
VincentNoc <vk6953@hotmail.com>
2024-05-14 15:13:32 +1200

    feat:Dark-Mode
    Summary: flatlaf default, so the default GUI doesn't look bad.

--- AVSCAN/src/main/java/com/mycompany/avscan/MainMenu.java
+++ AVSCAN/src/main/java/com/mycompany/avscan/MainMenu.java
@@ -289,8 +289,9 @@ public class MainMenu extends javax.swing.JFrame {
         /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
          * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
          */
-     
+        FlatLightLaf.setup();
         /* Create and display the form */
+        
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
                 new MainMenu().setVisible(true);
